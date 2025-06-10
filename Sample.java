86222ecc4b17245499f1fe08867640a0a1b8320f
Prateeshtalasu <prateeshtalasu1999@gmail.com>
2025-06-09 22:02:53 -0700

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,30 @@
 
 
 // Your code here along with comments explaining your approach
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast= head;
+        
+        while(fast!= null && fast.next!=null){
+            slow = slow.next;
+            fast= fast.next.next;
+            if(slow==fast){
+                
+                break;
+            }}
+            if(fast == null || fast.next == null) return null;
+            slow = head;
+            while(slow!=fast){
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+       
+        
+    
+}
+///////
+/// 
+/// 
